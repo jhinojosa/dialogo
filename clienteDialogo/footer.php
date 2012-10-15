bddaf421718396494d5523b1b0363336d0128ecc
cfarfan <camilo.farfan@usach.cl>
2012-10-15 17:59:02 -0300

    Actualización de footer.php
    Actualización de footer a la versión usada en las vistas

--- clienteDialogo/footer.php
+++ clienteDialogo/footer.php
@@ -9,26 +9,45 @@
                         Segundo Semestre de 2012</p>
                     </div>
                     <div id="usach-footer" class="span2">
-                        <a href="http://www.usach.cl/"><img src="../estilos/img/html/usach-white.png" alt="" /></a>
+                        <a href="http://www.usach.cl/"><img src="img/html/usach-white.png" alt="" /></a>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+
+    </div><!-- #page -->
+
     <div id="agradecimientos">
         <div class="container">
             <div class="row">
                 <div class="span12">
-                    <h6>Herramientas Utilizadas</h6>
-                    <p>Twitter Bootstrap, jQuery, Valums file uploader, Javascript SOAP Client 2.4, Tipsy, Jwysiwyg, JQuery context menu, JqTree, JEditable, JQuery Data Tables, NuSOAP PHP</p>
+                    <p>
+                        En el desarrollo de esta aplicación se utilizaron herramientas y bibliotecas licenciadas 
+                        bajo código abierto.
+                    </p>
+
+                    <a href="http://jquery.com/" target="_blank">jQuery</a>
+                    <a href="http://twitter.github.com/bootstrap/" target="_blank">Twitter Bootstrap</a>
+                    <a href="http://valums.com/ajax-upload/" target="_blank">Valums file uploader</a>
+                    <a href="http://javascriptsoapclient.codeplex.com/" target="_blank">Javascript SOAP Client 2.4</a>
+                    <a href="http://onehackoranother.com/projects/jquery/tipsy/" target="_blank">Tipsy</a>
+                    <a href="https://github.com/akzhan/jwysiwyg/" target="_blank">Jwysiwyg</a>
+                    <a href="http://medialize.github.com/jQuery-contextMenu/" target="_blank">JQuery context menu</a>
+                    <a href="http://mbraak.github.com/jqTree/" target="_blank">JqTree</a>
+                    <a href="http://www.appelsiini.net/projects/jeditable" target="_blank">JEditable</a>
+                    <a href="http://datatables.net/" target="_blank">JQuery Data Tables</a>
+                    <a href="http://sourceforge.net/projects/nusoap/" target="_blank">NuSOAP</a>
+                    <a href="http://www.php.net/" target="_blank">PHP</a>
                 </div>
             </div>
         </div>
     </div>
     
 <!-- javascript -->
-<script type="text/javascript" src="../estilos/js/bootstrap.js"></script>
-<script type="text/javascript" src="../estilos/js/less-1.3.0.min.js"></script>
+<script type="text/javascript" src="js/jquery-1.7.1.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.js"></script>
+<script type="text/javascript" src="js/less-1.3.0.min.js"></script>
+<script type="text/javascript" src="js/scripts.js"></script>
 <!-- /javascript -->
 
 </body>
