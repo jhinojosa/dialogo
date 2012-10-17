fbae1469b6f47c0c1a515d1a85cb99f50604f29f
cfarfan <camilo.farfan@usach.cl>
2012-10-17 00:04:16 -0300

    Solucionando Vistas Restantes
    ¬¬!!!

--- vistas/vista_9_10_11_configuracion.php
+++ vistas/vista_9_10_11_configuracion.php
@@ -1,17 +1,36 @@
 <?php include_once('header.php'); ?>
-<div id="barra-estado" class="row">
-          <div class="span12">
-            <ul class="breadcrumb">
-              <li><a href="#"><i class="icon-home"></i> Portada</a> <span class="divider">/</span></li>
-		<li><a href="#">Nuevo Diálogo</a> <span class="divider">/</span></li>
-              <li class="active">Configuración Diálogo</li>
-            </ul>
-          </div> <!-- #barra-estado -->
-        </div>
+<div id="menu-bar" class="row">
+    <div class="span12">
+        <div class="navbar">
+            <div class="navbar-inner">
+                <ul class="nav">
+                    <li ><a href="#"><i class="icon-home"></i> Portada</a></li>
+                    <li class="active" ><a href="#"><i class="icon-comment"></i> Nuevo Diálogo</a></li>
+                    <li ><a href="#"><i class="icon-star"></i> Marcadores</a></li>
+                    <li ><a href="#"><i class="icon-eye-open"></i> Intervenciones</a></li>
+                </ul>
+                <ul class="nav pull-right">
+                    <li><a href="#"><i class="icon-question-sign"></i> Ayuda</a></li>
+                    <li><a href="#"><i class="icon-info-sign"></i> Acerca de</a></li>
+                </ul>
+            </div>
+        </div>    
+    </div>  
+</div><!-- #menu-bar -->
 
-        <div class="row" id="content">
-          
-        </div><!-- #content -->
+<div id="barra-estado" class="row">
+    <div class="span12">
+        <ul class="breadcrumb">
+	           <li><a href="#"><i class="icon-home"></i> Portada</a> <span class="divider">/</span></li>
+	           <li><a href="#">Diálogos</a> <span class="divider">/</span></li>
+             <li><a href="#">Nuevo Diálogo</a> <span class="divider">/</span></li>
+        	   <li class="active">Configuración Diálogo</li>
+        </ul>
+    </div> <!-- #barra-estado -->
+</div>
+
+<div class="row" id="content">
+
 	<div id="main-content" class="span12">
 
 	  <h1>Configuración de Diálogo</h1>
@@ -124,11 +143,11 @@
 			</div>
              	</div>
 			
-         	<button class="btn btn-small pull-right"> Guardar Configuración</button>
-			<a class="btn btn-small pull-right" href="nuevo_dialogo.php"> Cancelar</a>
+         	<button class="btn btn-small btn-primary "> Guardar Configuración</button>
+			<a class="btn btn-small " href="nuevo_dialogo.php"> Cancelar</a>
                             
             </div>
 
         </div><!-- #main-content -->
-
+</div><!-- #content -->
 <?php include_once('footer.php'); ?>
